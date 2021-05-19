9b56afd137eb7f9208cf35a0d9aa8f92e42317f3
Pablo Oria de Rueda Pérez <pablo.oria1991@gmail.com>
2021-05-19 19:13:45 +0200

    Initial Commit

---
+++ HogwartsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HogwartsApp
+//
+//  Created by Pablo Oria de Rueda Pérez on 19/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
